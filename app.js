5c74bd3764d4939e8072e4dc444129b219d5bdc2
Erkan <erkan000@hotmail.com>
2022-08-16 14:56:33 +0300

    ss

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var port = 8080;
+
+// New code
+app.get('/test', function (req, res) {
+    res.send('the REST endpoint test run!');
+});
+
+
+app.listen(port, function() {
+  console.log('Server running at http://127.0.0.1:%s', port);
+});
